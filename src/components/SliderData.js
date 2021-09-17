25c2e0dd91a26f270795a89d5994cde18f1adc4d
unknown <Leo hengebaert>
2021-09-17 16:16:28 +0200

    modele site pour petit commerce

---
+++ src/components/SliderData.js
@@ -0,0 +1,21 @@
+export const SliderData = [
+    {
+      image: '/images/img-10.jpg'
+    },
+    {
+      image:
+      '/images/img-11.jpg'
+    },
+    {
+      image:
+      '/images/img-12.jpg'
+    },
+    {
+      image:
+      '/images/img-13.jpg'
+    },
+    {
+      image:
+      '/images/img-14.jpg'
+    }
+  ];
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from '../formulaire/Form'
+
+function SignUp() {
+  return (
+    <div>
+      <h1>S'inscrire</h1>
+      {/* <Form/> */}
+    </div>
+   
+  )
+}
+
+export default SignUp
